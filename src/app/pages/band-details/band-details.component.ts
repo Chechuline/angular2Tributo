b65dbd26bd35b1ade42c5c0973fa4c326eb4954e
Chechuline <jscentella@gmail.com>
2017-07-26 23:05:02 +0200

    band-details video tag
    Added video for dinamic videoId(Provisional)

--- src/app/pages/band-details/band-details.component.ts
+++ src/app/pages/band-details/band-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser'
 
 @Component({
   selector: 'app-band-details',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BandDetailsComponent implements OnInit {
 
-  constructor() { }
+  @Input() videoId;
+  videoUrl: SafeResourceUrl;
+  constructor(private sanitizer: DomSanitizer) {
+    this.videoId = '_Uu12zY01ts';
+  }
 
   ngOnInit() {
+    this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(`http://www.youtube.com/embed/${this.videoId}?html5=1&amp;rel=0&amp;hl=es_ES&amp;version=3`)
   }
 
 }
