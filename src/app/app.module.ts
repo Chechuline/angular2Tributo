9c07cf9a108d059a70ec734bd5e1660a4a512264
Chechuline <jscentella@gmail.com>
2017-07-26 16:31:29 +0200

    Added base components
    Added base components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UnderConstructionComponent } from './pages/under-construction/under-construction.component';
+import { BandDetailsComponent } from './pages/band-details/band-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    UnderConstructionComponent,
+    BandDetailsComponent,
   ],
   imports: [
     BrowserModule
